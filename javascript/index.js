f2d39031300b064533792498cf3c02c23e7c66ca
Toby <curti024@csusm.edu>
2016-07-23 21:42:01 -0700

    updated ui

--- javascript/index.js
+++ javascript/index.js
@@ -4,13 +4,14 @@ $( document ).ready
   { //morals and values
     var planets = ["The Sun", "Mercury", "Venus", "Earth", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"];
     var weightMultiples = [27.9, 0.377, 0.9032, 1, 0.1655, 0.3895, 2.640, 1.139, 0.917, 1.148, 0.06]; 
-    
+
     //populate the fields
     for(var i = 0; i < planets.length; i++)
     { 
       var option = $("<option></option>").text(planets[i]).val(weightMultiples[i]);
       $("#planetList").append(option);
     }
+
 
     window.getWeight = function()
     {
@@ -26,7 +27,7 @@ $( document ).ready
       $("#output").text("You would weigh " + answer + " on " + planet);
       
     
-    } 
+    };
     
     
     
